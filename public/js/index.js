7457825f98c42489896a193370c6377f047b2be4
stefanyalabbadii <156957457+stefanyalabbadii@users.noreply.github.com>
2024-04-22 02:44:17 -0400

    Delete public directory

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-let map;
-
-function initMap(){
-    map= new google.maps.Map(document.getElementById("map"),{
-        center:{lat:39.767943676509866, lng:-86.16000071202528},
-        zoom:13,
-    });
-
-}
